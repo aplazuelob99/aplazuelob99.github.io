30b0fff436366c6eca5b0a5a8b64b39e31aa8e3b
Adria Plazuelo <aplazuelob73@cepnet.net>
2021-02-11 15:17:50 +0100

    Menu navbar creado

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php 
+    
+        require_once("partials/menu.html"); 
+
+    ?>
+
+    
+
+    <div>
+        <!-- Accent-colored raised button with ripple -->
+        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">
+            Button
+        </button>
+    </div>
+    
+  
+</body>
+</html>
